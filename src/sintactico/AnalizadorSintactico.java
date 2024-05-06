d69a831162d31ed68d37d9ce8c4b06ed0265be71
Will9114 <waqq9114@gmail.com>
2024-05-06 02:34:44 -0600

    Metodos de asignacion

--- src/sintactico/AnalizadorSintactico.java
+++ src/sintactico/AnalizadorSintactico.java
@@ -121,4 +121,108 @@ public class AnalizadorSintactico {
         else
             return false;
     }
+    
+    private boolean operador_logico(){
+        if (tokens.get(puntero).getValorTablaTokens() == -31 || tokens.get(puntero).getValorTablaTokens() == -32 ||
+                tokens.get(puntero).getValorTablaTokens() == -33 || tokens.get(puntero).getValorTablaTokens() == -34 ||
+                		tokens.get(puntero).getValorTablaTokens() == -35 || tokens.get(puntero).getValorTablaTokens() == -36 ||
+                				tokens.get(puntero).getValorTablaTokens() == -41 || tokens.get(puntero).getValorTablaTokens() == -42 ||
+                						tokens.get(puntero).getValorTablaTokens() == -43)
+            return true;
+        else {
+            if (!ultimo)
+                System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": " +
+                        tokens.get(puntero).getLexema() + " no es un operador logico valido");
+            return false;
+        }
+    }
+
+    private boolean operador_aritmetico(){
+        if (tokens.get(puntero).getValorTablaTokens() == -21 || tokens.get(puntero).getValorTablaTokens() == -22 ||
+                tokens.get(puntero).getValorTablaTokens() == -24 || tokens.get(puntero).getValorTablaTokens() == -25)
+            return true;
+        else {
+            if (!ultimo)
+                System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": " +
+                        tokens.get(puntero).getLexema() + " no es un operador aritmetico valido");
+            return false;
+        }
+    }
+    
+    private boolean constante(){
+        if (tokens.get(puntero).getValorTablaTokens() == -61 || tokens.get(puntero).getValorTablaTokens() == -62 ||
+                tokens.get(puntero).getValorTablaTokens() == -63 || tokens.get(puntero).getValorTablaTokens() == -64 ||
+                		tokens.get(puntero).getValorTablaTokens() == -65)
+            return true;
+        else {
+            if (!ultimo)
+                System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": " +
+                        tokens.get(puntero).getLexema() + " no es una constante valida");
+            return false;
+        }
+    }
+    
+    private boolean asignacion(){
+    	if(identificador() && recorrer() && !ultimo) {
+    		if(tokens.get(puntero).getValorTablaTokens() == -26 && recorrer() && !ultimo) {
+    			if(expresion() && !ultimo) {
+    				if(tokens.get(puntero).getValorTablaTokens() == -75) {
+    					return true;
+    				} else {
+    					if (!ultimo)
+    						System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": Se esperaba un ';'");
+    				}
+    			}
+    		} else {
+    			if (!ultimo)
+    				System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": Se esperaba un ':='");
+    		}
+    	}
+    	return false;
+    }
+    
+    private boolean expresion() {
+    	if(termino() && !ultimo) {
+    		if(operador_logico() && recorrer() && !ultimo) {
+    			if(termino() && !ultimo) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
+    }
+
+
+	private boolean termino() {
+    	if(factor() && !ultimo) {
+    		if(operador_aritmetico() && recorrer() && !ultimo) {
+    			return termino();
+    		}
+    		return true;
+    	}
+    	return false;
+    }
+    
+    private boolean factor() {
+    	if((constante() && recorrer() && !ultimo) || (identificador() && recorrer() && !ultimo)){
+    		return true;
+    	} else if(tokens.get(puntero).getValorTablaTokens() == -73 && recorrer() && !ultimo) {
+    		if(expresion() && recorrer() && !ultimo) {
+    			if(tokens.get(puntero).getValorTablaTokens() == -74 && recorrer() && !ultimo) {
+    				return true;
+    			} else {
+    				if (!ultimo)
+    					System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": Se esperaba un ')'");
+    			}
+    		}
+    	} else if(tokens.get(puntero).getValorTablaTokens() == -43 && recorrer() && !ultimo) {
+    		if(factor()) {
+    			return true;
+    		}
+    	} else {
+    		if (!ultimo)
+    			System.err.println("Error en la linea " + tokens.get(puntero).getNumeroLinea() + ": Se esperaba un '(' o un '!'");
+    	}
+    	return false;
+    }
 }
